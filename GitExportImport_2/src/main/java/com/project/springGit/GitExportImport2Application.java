9e3432bc57e88e38691151119c3da1c5c3ebe3dc
KiranDulam <kiransaichandra@gmail.com>
2021-06-07 02:32:25 -0400

    GITExportImport2 commit msg1

---
+++ GitExportImport_2/src/main/java/com/project/springGit/GitExportImport2Application.java
@@ -0,0 +1,13 @@
+package com.project.springGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExportImport2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitExportImport2Application.class, args);
+	}
+
+}
---
+++ GitExportImport_2/src/test/java/com/project/springGit/GitExportImport2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExportImport2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
